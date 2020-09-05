0f9c804b1f83bb28084514201eb5868903b81b99
Christian Hellsten <christian@aktagon.com>
2020-09-05 12:07:51 +0300

    Change cron image to use Alpine Linux
    Include log output in Slack notification

--- cron/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Starting cron..."
-printenv | grep -v "no_proxy" >> /etc/environment
-cron -f
--- cron/run.sh
+++ cron/run.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-# TODO: Replace with the cron command you want to run
-echo "it works"
+# TODO: Replace this line with the cron command you want to run. Remember to
+# write stdout and stderr to the log file.
+echo "it works" &> /var/log/app.log
 
 if [ $? -eq 0 ]
 then
-  echo "Ran cron job successfully"
+  curl -X POST -H 'Content-Type: application/json' --data "{\"text\":\"Ran cron job successfully: $(tail -n 5 /var/log/app.log)\"}" $SLACK_URL
   exit 0
 else
-  # TODO: Replace this with a Slack hook URL to get notified of exceptions
-  curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself!"}' https://hooks.slack.com/<LOL>
+  curl -X POST -H 'Content-Type: application/json' --data "{\"text\":\"Failed to run cron job: $(tail -n 1 /var/log/app.log)\"}" $SLACK_URL
   exit 1
 fi
-
